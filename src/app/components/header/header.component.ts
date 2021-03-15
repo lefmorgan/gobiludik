a3475513227f3925773bb1fa94c0a0900c342768
morgan <lef.morgan@gmail.com>
2021-03-15 11:48:34 +0100

    start home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  public user: User;
+  constructor() {}
 
   ngOnInit(): void {
+    let userLinea = localStorage.getItem('user');
+    this.user = JSON.parse(userLinea);
   }
-
 }
